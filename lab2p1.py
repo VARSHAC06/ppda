f6989bc0c3ab50268915852301a8be6f127402c7
VARSHAC06 <varshacbca24@rvu.edu.in>
2025-01-31 11:04:24 +0530

    Add files via upload

---
+++ lab2p1.py
@@ -0,0 +1,9 @@
+# BCA Student, SoCSE
+# This program prints important messages.
+print("Hello, world!")
+""" Here starts the
+docstring
+"""
+print() # blank line
+print("Suppose two swallows \"carry\" it together.") # to print "
+print('African or "European" swallows?') # single and double quotes
